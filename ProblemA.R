afe558d584104664702088ae3d1ae367d3d4cd49
bryce-jacobs <47409450+bryce-jacobs@users.noreply.github.com>
2020-01-24 21:32:30 -0800

    Add files via upload
    Cleaned up some of the text and variable names. Still missing PC1 vs PC2 plotting.

--- ProblemA.R
+++ ProblemA.R
@@ -1,12 +1,17 @@
-forestdataframe <- read.table("/Users/megbobba/Desktop/covtype.data", sep=',')
+forestdataframe <- read.table("covtype.data", sep=',')
 forestdata <- as.matrix(forestdataframe)
 tpca <- prcomp(forestdata[, (1:54)])
 tmp <- cumsum(tpca$sdev^2)
 percentageOfTotalVariance <- tmp / tmp[54]
 plot(percentageOfTotalVariance)
-# As we can see from the plot, the first principal component accounts for 
-# 66 percent of the variance and the second principal component accounts for
-# 96 percent. Given that the first two components account for most of the 
-# variance and we are analyzing the data for a class project, we think
-# the that these principal components are enough. However, if we were a
-# company such as Amazon or Google, perhaps we would want up to four.
+
+#As we can see from the plot, the first principal component accounts for
+#66 percent of the variance and the second principal component accounts for
+#96 percent. The instructions for this assignment ask about how many are "enough"
+#because the precision we need depends on what we are trying to do. Given that
+#the first two components account for most of the variance and we are analyzing
+#the data for a class project, we think the that these principal components are
+#enough. However, if we were a company such as Amazon or Google, perhaps we
+#would want up to four principal compoments (as we would have more computational
+#resources and customers who would rely on our precision).
+
